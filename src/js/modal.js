6d819b6a60d13f0fcff54e610e85564eb21bf7d2
George Stavroulakis <g.stavroulakis@gmail.com>
2022-02-04 11:45:17 +0100

    add modal window listeners

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,30 @@
 export default function test() {
-	console.log("test");
+	const buttonRegister = document.querySelector(".btn--register"),
+		modal = document.getElementById("modalWindow"),
+		btnClose = document.querySelector(".modal--close"),
+		overlay = document.querySelector(".overlay");
+
+	//calls the modal window
+	buttonRegister.addEventListener("click", (e) => {
+		if (modal.classList.contains("hidden")) {
+			modal.classList.remove("hidden");
+		}
+		if (overlay.classList.contains("hidden")) {
+			overlay.classList.remove("hidden");
+		}
+	});
+	//close modal function
+	const close = () => {
+		modal.classList.add("hidden");
+		overlay.classList.add("hidden");
+	};
+	//close the modal window listeners
+	btnClose.addEventListener("click", close);
+	// close modal if click is in the gray overlay
+	overlay.addEventListener("click", close);
+	//add event listener to close the modal window when esc is pressed down
+	document.addEventListener("keydown", function (e) {
+		e.preventDefault;
+		if (e.key === "Escape" && !modal.classList.contains("hidden")) close();
+	});
 }
